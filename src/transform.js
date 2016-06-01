fb5e82e56bed18c178adf853bbc4418429007abd
jncr <john@byers.it>
2016-06-01 13:48:48 +0100

    not error on fields missing from response

--- src/transform.js
+++ src/transform.js
@@ -4,6 +4,13 @@ _.mixin(require('underscore.deep'));
 
 module.exports.transform = function(response, schema) {
     var instance = instantiator.instantiate(schema);
+    var flatInstance = _.deepToFlat(instance);
+    for (var key in flatInstance){
+        if(!_.deepHas(response, key)) {
+            delete flatInstance[key];
+        }
+    }
+    instance = _.deepFromFlat(flatInstance);
     var keys = _.deepKeys(instance);
     var output = _.deepPick(response, keys);
     return output;
